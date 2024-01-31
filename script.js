3c9f3b1298ce410ae7df197e6d419db07bd1ee3b
Travis J <tfauvelle@gmail.com>
2024-01-31 17:47:20 -0500

    passing all test cases except for last two

--- script.js
+++ script.js
@@ -1,22 +1,59 @@
 const convertBtn = document.getElementById("convert-btn");
 const outputDisplay = document.getElementById("output");
 const numberInput = document.getElementById("number");
+const romanNumerals = [
+  "M",
+  "CM",
+  "D",
+  "CD",
+  "C",
+  "XC",
+  "L",
+  "XL",
+  "X",
+  "IX",
+  "V",
+  "IV",
+  "I",
+];
+const decimalNumbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
 const validateInput = () => {
+  let flag = true;
   let inputValue = numberInput.value;
-  let result = "valid input";
 
   if (inputValue === "") {
     outputDisplay.innerText = "Please enter a valid number";
+    flag = false;
   } else if (inputValue < 1) {
     outputDisplay.innerText =
       "Please enter a number greater than or equal to 1";
+    flag = false;
   } else if (inputValue >= 4000) {
     outputDisplay.innerText =
       "Please enter a number less than or equal to 3999.";
-  } else {
-    outputDisplay.innerText = result;
+    flag = false;
   }
+
+  if (flag) {
+    convertToRoman(inputValue);
+  }
+};
+
+const convertToRoman = (input) => {
+  let result = "";
+  let inputValue = input;
+
+  while (inputValue > 0) {
+    for (let i = 0; i < decimalNumbers.length; i++) {
+      if (decimalNumbers[i] <= inputValue) {
+        result += romanNumerals[i];
+        inputValue -= decimalNumbers[i];
+      }
+    }
+  }
+
+  outputDisplay.innerText = result;
 };
 
 convertBtn.addEventListener("click", validateInput);
